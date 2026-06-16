91127e9a622a3d6b9c9546889f3bcc34b6990131
Arwa <arwa.omarsait@gmail.com>
2026-06-17 01:04:57 +0300

    delet task

--- readTask.js
+++ readTask.js
@@ -13,6 +13,9 @@ function readTask() {
   // 1. استهداف وتفريغ الحاوية الرئيسية الكبيرة
   document.getElementById("tasks_container").innerHTML = "";
 
+  //متغير احفظ فيه اندكس كل مهمة حتى استطيع التميز بينهم
+  var index = 0;
+
   // 2. تكرار المصفوفة لبناء المهام
   for (task of tasks) {
     let content = `
@@ -28,7 +31,7 @@ function readTask() {
       <button class="circular_btn" id="edit_btn">
         <span class="material-symbols-outlined"> edit </span>
       </button>
-      <button class="circular_btn" id="delet_btn">
+      <button class="circular_btn" id="delet_btn" onclick = "deletTask(${index})">
         <span class="material-symbols-outlined"> delete </span>
       </button>
     </div>
@@ -37,26 +40,12 @@ function readTask() {
 
     // 3. إضافة الـ HTML الجديد بداخل الحاوية الكبيرة
     document.getElementById("tasks_container").innerHTML += content;
+    index++;
   }
 }
 
-/*function addTask() {
-  document
-    .getElementById("add_task_btn")
-    .addEventListener("click", function () {
-      let newtask = prompt("ادخل المهمة");
-      console.log(newtask);
-      let newTaskObj = {
-        title: newtask,
-        isDone: false,
-      };
-      tasks.push(newTaskObj);
-      readTask();
-    });
-}*/
-
 function addTask() {
-  const modal = document.getElementById("custom_prompt");
+  const modal = document.getElementById("add_task_prompt");
   const taskInput = document.getElementById("task_input");
 
   // 1. عند الضغط على زر "إضافة مهمة" الرئيسي بالصفحة: اظهر النافذة
@@ -75,7 +64,6 @@ function addTask() {
       let newtask = taskInput.value.trim();
 
       if (newtask !== "") {
-        console.log(newtask);
         let newTaskObj = {
           title: newtask,
           isDone: false,
@@ -94,6 +82,32 @@ function addTask() {
     .addEventListener("click", function () {
       modal.style.display = "none"; // إخفاء النافذة دون حفظ
     });
+}
+
+function deletTask(index) {
+  // 1. استهداف عنصر الـ Modal الأصلي الموجود في الـ HTML
+  const modal = document.getElementById("delet_task_prompt");
+
+  // 2. تحديث نص السؤال ليظهر اسم المهمة المراد حذفها ديناميكياً
+  modal.querySelector("h3").innerText =
+    `هل انت متأكد من رغبتك في حذف المهمة (${tasks[index].title})؟`;
+
+  // 3. إظهار النافذة المنبثقة
+  modal.style.display = "flex";
+
+  // 4. عند الضغط على زر "نعم" داخل النافذة المنبثقة
+  const deleteBtn = document.getElementById("delet_task_btn");
+  deleteBtn.onclick = function () {
+    tasks.splice(index, 1); // حذف العنصر من المصفوفة
+    readTask(); // إعادة بناء قائمة المهام
+    modal.style.display = "none"; // إخفاء النافذة
+  };
+
+  // 5. عند الضغط على زر "لا" (إلغاء)
+  const cancelBtn = document.getElementById("undelet_task_btn");
+  cancelBtn.onclick = function () {
+    modal.style.display = "none"; // إخفاء النافذة دون حذف
+  };
 }
 
 readTask();
